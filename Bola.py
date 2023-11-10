82de550d59c8be47086ad40cd70da48fbf603067
Maulanana <149945083+Maulanana@users.noreply.github.com>
2023-11-11 00:18:16 +0700

    Bola.py

---
+++ Bola.py
@@ -0,0 +1,26 @@
+print("Aplikasi Menghitung Luas dan Volume Bola")
+
+"""
+Nama  : Maulana
+NIM   : 220511177
+Kelas : TI22K
+PBO   : Tugas Pertemuan 1
+
+Keterangan:
+phi = 3.14
+r = Jari - jari
+"""
+
+# Atur Nilai3
+phi = 3.14
+r = 15
+
+# Rumus
+luas_bola = 4 * phi * r**2
+volume_bola = 4/3 * phi * r**3
+
+# Output
+print("Phi:", phi)
+print("Jari - Jari:", r)
+print("Luas:", luas_bola)
+print("Volume:", volume_bola)
