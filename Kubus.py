a95cbeef4913daf9e1744e9baaa180d50572983d
Maulanana <149945083+Maulanana@users.noreply.github.com>
2023-11-11 00:24:57 +0700

    Kubus.py

---
+++ Kubus.py
@@ -0,0 +1,23 @@
+print("Aplikasi Menghitung Luas dan Volume Kubus")
+
+"""
+Nama  : Maulana
+NIM   : 220511177
+Kelas : TI22K
+PBO   : Tugas Pertemuan 1
+
+Keterangan:
+r = Rusuk
+"""
+
+# Atur Nilai
+r = 3
+
+# Rumus
+luas = 6 * r**2
+volume = r**3
+
+# output
+print("Rusuk:", r)
+print("Luas:", luas)
+print("Volume:", volume)
